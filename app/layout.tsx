460f8382b10f468dcdbf3557796677b2d5a8184a
Prasannakumar Kapate <prasanna.1616@gmail.com>
2024-07-22 15:08:59 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Careplus",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
