d14c4e75c6918554915fb100ad81b0a7f48ff18c
Prasanna Kapate <prasanna.1616@gmail.com>
2024-07-22 18:01:48 +0530

    added form library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO add OTP verification */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -12,8 +15,26 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit-content mx-auto"
           />
+
+          <PatientForm />
+          <div className="text-14-regular">
+            <p className="justify-item-end text-dark-600 xl:text-left">
+              © 2024 CarePlus. All Rights Reserved.
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="onboarding"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
